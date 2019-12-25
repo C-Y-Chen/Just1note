218ac380f018e82696bd656463c49b96bb08899f
陳智育 <chenzhiyu@chenzhiyude-MacBook-Pro.local>
2019-12-25 23:02:12 +0800

    sqs

---
+++ sqs/sqs.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require 'vendor/autoload.php';
+
+use Aws\Sqs\SqsClient;
+use Aws\Exception\AwsException;
+
+class AmazonSQS
+{
+    private $config;
+    private $client;
+
+    public function __construct()
+    {
+      $this->client = SqsClient::factory($config); // config是你的aws的credentials
+    }
+
+    public function sendQueueMsg($queue_url, $msg_body)
+    {
+        $params = [
+            'QueueUrl' => $queue_url,
+            'MessageBody' => $msg_body
+        ];
+        $result = $this->client->sendMessage($params);
+    }
+}
